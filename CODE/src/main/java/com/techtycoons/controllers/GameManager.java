7c1a7c5c3ba2c5efe163d911aa117deb3b214422
Matt Janny <mattjanny@Matts-MacBook-Pro.local>
2019-11-09 13:46:10 -0500

    More Progress! guess letter and words

--- CODE/src/main/java/com/techtycoons/controllers/GameManager.java
+++ CODE/src/main/java/com/techtycoons/controllers/GameManager.java
@@ -1,6 +1,9 @@
 package com.techtycoons.controllers;
 
 import com.techtycoons.controllers.GameplayFunctions;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /*
  * GameManager Class
@@ -12,6 +15,7 @@ public class GameManager {
 	int correct_guesses;
 	String letters_guessed;
 	String[] words_guessed;
+	static Queue<String> guessQueue;
 	
 	
 	/*
@@ -22,6 +26,7 @@ public class GameManager {
         this.correct_guesses   = 0;
         this.letters_guessed   = null;
         this.words_guessed = new String[6]; //Max amount of word guesses can only be 6
+        this.guessQueue = new LinkedList<>();
 	}
 	
 	/*
@@ -43,8 +48,16 @@ public class GameManager {
 	}
 	
 	/*
+	 * Method: newGuess
+	 * Used for the controller to send all guesses to the guess queue
+	 */
+	void newGuess(String guess) {
+		guessQueue.add(guess);
+	}
+	
+	/*
 	 * Method: pickWord()
-	 * PLACEHOLDER FOR METHOD TO PICK OUR WORD RANDOMLY FROM THE LIST
+	 * PLACEHOLDER FOR METHOD TO PICK OUR WORD RANDOMLY FROM OUR TEXT FILE LIST
 	 */
 	
 	
@@ -55,6 +68,32 @@ public class GameManager {
 	public static void main(String[] args) {
 		//String word = pickWord();
 		String word = "hello"; //for test purposes now.
+		GameplayFunctions wordActions = new GameplayFunctions (word);
+		
+		Boolean still_playing = true;
+		Boolean did_you_win = false;
+		
+		while (still_playing) {
+			if (guessQueue.size() > 0) {
+				String guess = guessQueue.poll();
+				
+				//word guess
+				if (guess.length() > 1) {
+					did_you_win = wordActions.guessWord(guess);
+				}
+				//letter guess
+				else if (guess.length() == 1) {
+					char[] letter = new char[guess.length()];
+					letter[0] = guess.charAt(0);
+					List<Integer> places = wordActions.guessLetter(letter[0]);
+					
+					
+				}
+				else {
+					still_playing = false; //should never happen
+				}
+			}
+		}
 		
 	}
 
