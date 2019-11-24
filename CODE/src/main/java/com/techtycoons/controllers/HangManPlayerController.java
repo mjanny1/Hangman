ebad100d5c4f7842f54d1910d308c947eedbac19
Joyatee Chattopadhyay <jchattop@villanova.edu>
2019-11-24 14:07:59 -0500

    HangManPlayerController changes

--- CODE/src/main/java/com/techtycoons/controllers/HangManPlayerController.java
+++ CODE/src/main/java/com/techtycoons/controllers/HangManPlayerController.java
@@ -2,10 +2,16 @@ package com.techtycoons.controllers;
  
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+
 import com.techtycoons.model.UserWord;
+import com.techtycoons.model.UserWordAndBlank;
 import com.techtycoons.services.HangManPlayerService;
  
 @Controller
@@ -26,8 +36,8 @@ public class HangManPlayerController {
 	@Autowired
 	GameManager gm;
 	
-	@Autowired
-	GameplayFunctions gf;
+//	@Autowired
+//	GameplayFunctions gf;
 	
 	
 	
@@ -40,39 +50,92 @@ public class HangManPlayerController {
     and giving the required blank space for the word to be guessed  */
    @RequestMapping(value="/singleUser", method = RequestMethod.GET)
     public String singleModeHome(Model m) throws IOException{
-	    Thread newGame = new Thread(new GameManager());
-	    newGame.start();
-	    //String received=hmpService.fetchEasyWords();
-	    //String blankSpace=  gm.makeBlankWord(received);
-	    String blankSpace = gm.getWordWithGuesses();
-	    String received = gm.getWord();
+	    //Thread newGame = new Thread((Runnable) new GameManager());
+	    //newGame.start();
+	    //gm.run();
+	    String received=hmpService.fetchEasyWords();
+	    String blankSpace=  gm.makeBlankWord(received);
+	   // String blankSpace = gm.getWordWithGuesses();
+	    //String received = gm.getWord();
+	   // System.out.println("blankSpace:"+blankSpace+"received:"+received);
 	    m.addAttribute("word", blankSpace);
-	    UserWord userWord = new UserWord("joyatee", received);
-	    List<UserWord> list = new ArrayList<UserWord>();
+	   
+	   UserWordAndBlank userWord = new UserWordAndBlank(received,blankSpace);
+	    List<UserWordAndBlank> list = new ArrayList<UserWordAndBlank>();
+	    
 	    list.add(userWord);
-	    ObjectMapper Obj = new ObjectMapper(); 
+	    
+	    JSONObject obj=new JSONObject();
+	    obj.put("received", received);
+		obj.put("blankSpace", blankSpace);
+		FileWriter file=new FileWriter("E:\\usersRandomWordsNew.json");
+		file.write(obj.toString());
+		file.flush();
+	    System.out.println("json object is::"+obj);
+	    
+	   // list.add(blankSpace);
+	   /* ObjectMapper Obj = new ObjectMapper(); 
+	    
 	    String jsonStr = Obj.writeValueAsString(list); 
 	    
         // Displaying JSON String 
         System.out.println(jsonStr); 
 	    
-	    File file = new File("E:\\userswords.json");
+	    File file = new File("E:\\usersRandomWordsq.json");
 	    
 	    FileOutputStream fout = new FileOutputStream(file, true);
 	    fout.write(jsonStr.getBytes());
-	    fout.close();
+	    fout.close();*/
         return "singleUser";
     }
    
    @RequestMapping(value="/guessLetterOrWord", method = RequestMethod.POST)
-   public void guessLetterOrWord(@RequestParam("letterOrWord") String letterOrWord,Model m) throws IOException{
+   public String guessLetterOrWord(Model t,@RequestParam("letterOrWord") String letterOrWord,@RequestParam("modified1") String modified1,Model m) throws IOException{
 	  	   System.out.println("Guessed Letter is::"+letterOrWord);
+	  	 String wordFetched=null;
+	  	String blankSpace=null;
+	  	String word,word1,word2=null;
+
 	  	 //   gf.guessLetterOrWord(letterOrWord, received, blankSpace);
-	  	   gm.newGuess(letterOrWord);
+	  	   //gm.newGuess(letterOrWord);
+	  	  // gm.getWordWithGuesses();
+	  	   JSONParser parse=new JSONParser();
+	  	   
+	  	   try {
+	  		//   parse.parse(arg0)
+	  		 Object obj=parse.parse(new FileReader("E:\\usersRandomWordsNew.json"));
+			JSONObject json= (JSONObject) obj;
+			wordFetched=(String)json.get("received");
+			blankSpace=(String)json.get("blankSpace");
+			//System.out.println("Word is *******************"+wordFetched+"::blank Space is::"+blankSpace);
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  	  // System.out.println("modified1::"+modified1);
+	  	  //for(int i=0;i<=wordFetched.length();i++) {
+		word=gm.guessLetterOrWord(letterOrWord, wordFetched, blankSpace);
+		word1=gm.guessLetterOrWord(letterOrWord, wordFetched, modified1);
+	//	word2=gm.guessLetterOrWord(wronguess, wordFetched, modified1);
+		
+		
+		
+		
+		
+	  	  //}
+	  	  m.addAttribute("word", word);
+	  	  t.addAttribute("word", word1);
+	  	 // x.addAttribute("wrongGuess", word2);
+	  	  
+		 return "singleUser";
+   }
+	  	  //  gf.guessLetterOrWord(letterOrWord, received, blankSpace);
+	  	 //  gm.fillInTheBlanks(letterOrWord, word, spots)
+
 	  	   
 	  	   
 	    
    }
     
  
-}
