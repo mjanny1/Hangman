d5301ad86afc908c2b103f82242f8ba276e8f8c1
Matt Janny <mattjanny@Matts-MacBook-Pro.local>
2019-11-29 15:02:55 -0500

    Fixing User Record Reader

--- CODE/src/main/java/com/techtycoons/services/UserDataHandler.java
+++ CODE/src/main/java/com/techtycoons/services/UserDataHandler.java
@@ -3,6 +3,9 @@ package com.techtycoons.services;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
 import java.io.FileReader;
 
 public class UserDataHandler {
@@ -25,19 +28,24 @@ public class UserDataHandler {
 		try {
 			userObject = null;
 			JSONParser parser = new JSONParser();
-			FileReader file = new FileReader("../../../../resources/config/user_records.json");
+			String filePath = ResourceUtils.getFile("classpath:config/user_records.json").getAbsolutePath();
+			System.out.println("Record File Path = " + filePath);
+			FileReader file = new FileReader(filePath);
 			Object obj = parser.parse(file);
+			//JSONObject userData = (JSONObject) obj;
 			JSONArray userData = (JSONArray) obj;
 			userData.forEach( user -> findUsername(username, (JSONObject) user) );
 			
 			if (userObject == null) {
 				userRecord = "Could not locate User's Record!";
+				System.out.println("UserDataHandler: getUserRecord: Could not locate User's Record!");
 			}
 			else
 			{
-				int wins = (int) userObject.get("wins");
-				int losses = (int) userObject.get("losses");
-				userRecord = "Record: " + Integer.toString(wins) + "-" + Integer.toString(losses);
+				long wins = (long) userObject.get("wins");
+				long losses = (long) userObject.get("losses");
+				userRecord = "Record: " + Long.toString(wins) + "-" + Long.toString(losses);
+				System.out.println(userRecord);
 			}
 		}
 		catch (Exception ex) {
@@ -51,11 +59,15 @@ public class UserDataHandler {
 	 * Locates the record for the user in the file
 	 */
 	private void findUsername(String username, JSONObject obj) {
+		System.out.println(obj.get("username"));
 		String objectUsername = (String) obj.get("username");
-		if (objectUsername == username) {
+		System.out.println("Username: " + username + "   ObjectUserName: " + objectUsername);
+		if (objectUsername.contentEquals(username) ) {
+			System.out.println("Found Match");
 			userObject = obj;
 		}
 		else {
+			System.out.println("Match Not Found");
 			userObject = null;
 		}
 		
